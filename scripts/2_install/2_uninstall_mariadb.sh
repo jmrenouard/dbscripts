9dfd63eb50195683c427df5e3fa6c1a6522e7666
Jean-Marie RENOUARD <jmrenoaurd@gmail.com>
2020-10-07 00:24:10 +0200

    Script de desinstalaltion

---
+++ scripts/2_install/2_uninstall_mariadb.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
+lRC=0
+VERSION=10.5
+
+banner "BEGIN SCRIPT: $_NAME"
+
+cmd 'rm -f /etc/yum.repos.d/mariadb_*.repo'
+
+cmd "yum -y update"
+lRC=$(($lRC + $?))
+
+cmd "yum -y remove mysql-server mariadb-server MariaDB-backup MariaDB-client MariaDB-compat socat jemalloc  nmap lsof perl-DBI nc mariadb-server-utils pigz perl-DBD-MySQL perl-DBI percona-toolkit mysqlreport"
+lRC=$(($lRC + $?))
+
+[ -d "/opt/local/MySQLTuner-perl" ] && cmd "rm -rf /opt/local/MySQLTuner-perl"
+
+
+cmd "rm -f /etc/profile.d/mysqltuner.sh"
+
+footer "END SCRIPT: $NAME"
+exit $lRC
