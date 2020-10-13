122b3e1e2a41817c4d48c2487325928c187ed1d9
Jean-Marie RENOUARD <jmrenoaurd@gmail.com>
2020-10-13 19:13:23 +0200

    Adding sys schema for MAriaDB

--- scripts/2_install/3_config_start.sh
+++ scripts/2_install/3_config_start.sh
@@ -44,5 +44,12 @@ cmd "journalctl -xe -o cat -u mariadb"
 
 cmd "tail -n 15 /var/lib/mysql/mysqld.log"
 
+
+cd /opt/local
+cmd "git clone https://github.com/FromDual/mariadb-sys.git"
+lRC=$(($lRC + $?))
+cd /opt/local/mariadb-sys
+mysql -f < sys_10.sql
+
 footer "END SCRIPT: $NAME"
 exit $lRC
