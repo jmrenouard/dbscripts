b32771de29226ef2bcf4118dc742fca707b9ad6d
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-11-06 14:00:06 +0100

    Update publish.sh

--- vms/publish.sh
+++ vms/publish.sh
@@ -8,15 +8,20 @@ VERSION=8.0.2
 
 [ -f ".envtoken" ] && $VAGRANT cloud auth login --token=$(cat .envtoken)
 set -x
-rm -f $BOX
-$VAGRANT package --output $BOX
+if [ ! -f "$BOX" ] ; then
+	#rm -f $BOX
+	$VAGRANT package --output $BOX
+fi
 MD5SUM_BOX=$(md5sum $BOX|awk '{print $1}')
 
 $VAGRANT cloud box show $CLOUD_BOX
 
 $VAGRANT cloud publish $CLOUD_BOX $VERSION \
- virtualbox $BOX -d "Version v$VERSION" --release --version-description "Version from $(date)" \
---clean --checksum-type md5 --checksum $MD5SUM_BOX
+ virtualbox $BOX \
+ --box-version $VERSION \
+ -d "Version v$VERSION" --release \
+ --version-description "Version from $(date)" \
+--checksum-type md5 --checksum $MD5SUM_BOX --force --debug
 
 #$VAGRANT cloud version release $CLOUD_BOX $VERSION
 
