229e90377673aea4cf83aab88ea5cd405fd1b847
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-11-23 15:38:17 +0000

    Script reset

---
+++ scripts/2_install/10_reset_galera_node.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
+lRC=
+banner "BEGIN SCRIPT: $_NAME"
+
+cmd "systemctl stop mariadb"
+
+cmd "rm -f ${DATADIR}/galera.cache ${DATADIR}/grastate.dat ${DATADIR}/gvwstate.dat"
+
+cmd "systemctl start mariadb"
+
+footer "END SCRIPT: $NAME"
+exit $lRC
