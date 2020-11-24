51f7916190b8355d1e42ca2627d94206c29ddf5b
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-11-24 19:23:32 +0000

    Adding telnet and mysql clinets

--- vms/provision_generic.sh
+++ vms/provision_generic.sh
@@ -83,7 +83,7 @@ sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.
 echo '---------------------------------------------------'
 echo "7° Install some utlities"
 echo '---------------------------------------------------'
-sudo yum -y install rsync lftp socat htop python3 time git
+sudo yum -y install rsync lftp socat htop python3 time git telnet mysql
 
 echo '---------------------------------------------------'
 echo "8° update distro"
