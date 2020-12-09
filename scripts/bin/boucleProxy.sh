2ac4ef518fc219dd4a8ff9ff0491304725cce865
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-12-09 15:42:05 +0100

    Update boucleProxy.sh

--- scripts/bin/boucleProxy.sh
+++ scripts/bin/boucleProxy.sh
@@ -2,10 +2,10 @@
 
 [ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
 
-pass=$(get_ssh_mariadb_root_password dbsrv1)
-srvProxy=proxy1
+pass=$(get_ssh_mariadb_root_password jmr_dbsrv1)
+srvProxy=jmr_proxy
 
-for i in $(seq 1 500); do 
+for i in $(seq 1 500); do
 	mysql -h$srvProxy -uroot -p$pass mysql -e 'show variables like "report_host%"' -Nrs
 	sleep 1s
 	echo "------"
