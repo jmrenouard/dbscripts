f9214aa2dd2bd007e11674e9ab4026f72ed752d8
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-12-09 15:43:34 +0100

    Update boucleProxy.sh

--- scripts/bin/boucleProxy.sh
+++ scripts/bin/boucleProxy.sh
@@ -6,7 +6,7 @@ pass=$(get_ssh_mariadb_root_password jmr_dbsrv1)
 srvProxy=jmr_proxy
 
 for i in $(seq 1 500); do
-	mysql -h$srvProxy -uroot -p$pass mysql -e 'show variables like "report_host%"' -Nrs
+	mysql -h$srvProxy -uroot -p$pass mysql -e 'show variables like "report_host%"' -Nrs 2>/dev/null
 	sleep 1s
 	echo "------"
 done
