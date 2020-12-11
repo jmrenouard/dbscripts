ab1ca7522eb9834b19dcaeceb679802b868fd465
Jean-Marie Renouard <jmrenouard@gmail.com>
2020-12-11 13:29:33 +0100

    Update mbsetup_slave.sh

--- scripts/bin/mbsetup_slave.sh
+++ scripts/bin/mbsetup_slave.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
 master=$1
+if [ -z "$master" ]; then
+	error "Please give a master host"
+	exit 127
+fi
+
 ruser=${2:-"replication"}
 pass=$3
 [ -z "$pass" ] && pass=$(ssh -q $master "check_user_passwords.sh"|  grep $ruser| awk '{print $3}')
 
 datadir=/var/lib/mysql
-
 systemctl stop mariadb
+if [ -f "/etc/my.cnf.d/999_galera_settings.cnf" ]; then
+	mv /etc/my.cnf.d/999_galera_settings.cnf /etc/my.cnf.d/999_galera_settings.cnf.disabled
+fi
 
 rm -rf $datadir/*
 
@@ -15,8 +24,8 @@ cd $datadir
 ssh -q $master "mariabackup --user=root --backup --stream=mbstream" | mbstream -v -x
 
 ls -ls
-rfile=$(awk '{print $1}' xtrabackup_binlog_info) 
-posrfile=$(awk '{print $2}' xtrabackup_binlog_info) 
+rfile=$(awk '{print $1}' xtrabackup_binlog_info)
+posrfile=$(awk '{print $2}' xtrabackup_binlog_info)
 systemctl start mariadb
 # ...
 mysql -e  "CHANGE MASTER TO MASTER_HOST='$master', MASTER_USER='$ruser', MASTER_PORT=3306, MASTER_LOG_FILE='$rfile', MASTER_LOG_POS=$posrfile"
