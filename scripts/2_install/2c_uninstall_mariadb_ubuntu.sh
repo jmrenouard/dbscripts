6032f7ef3d16d30a3524cac577c0b1ea60a098a4
Jean-Marie RENOUARD <jmrenouard@gmail.com>
2021-05-07 14:23:05 +0200

    Adding removing script for ubuntu

---
+++ scripts/2_install/2c_uninstall_mariadb_ubuntu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
+lRC=0
+VERSION=10.5
+
+banner "BEGIN SCRIPT: $_NAME"
+
+find /etc/apt/sources.list.d -type f -iname '*mariadb*.list' -exec rm -f {} \;
+
+cmd "apt -y update" "UPDATE PACKAGE LIST"
+
+cmd "apt -y remove mariadb-client mariadb-backup mariadb-server mariadb-plugin-cracklib-password-check mariadb-plugin-connect socat libjemalloc2 nmap pwgen lsof pigz percona-toolkit"
+lRC=$(($lRC + $?))
+
+[ -d "/opt/local/MySQLTuner-perl" ] && cmd "rm -rf /opt/local/MySQLTuner-perl"
+
+
+cmd "rm -f /etc/profile.d/mysqltuner.sh"
+
+footer "END SCRIPT: $NAME"
+exit $lRC
