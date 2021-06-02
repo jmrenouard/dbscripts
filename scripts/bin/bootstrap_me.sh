190d125c515de93a80920f9e56f01e46f5993066
Jean-Marie Renouard <jmrenoaurd@gmail.com>
2021-06-02 18:54:25 +0200

    Script de bootstrap after crash

---
+++ scripts/bin/bootstrap_me.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+[ -f '/etc/bootstrap.conf' ] && source /etc/bootstrap.conf
+
+perl -i -pe 's/safe_to_bootstrap: 0/safe_to_bootstrap: 1/g' /var/lib/mysql/grastate.dat 
+/usr/bin/galera_new_cluster
+
+for srv in $(echo $node_addresses| tr ',' ' '); do
+	[ "$my_private_ipv4" = "$srv" ] && continue
+	echo "* RESTARTING $srv"
+	ssh -q $srv "systemctl restart mysql"
+done 
