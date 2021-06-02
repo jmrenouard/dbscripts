00ef2d14afa24ac8c85c9ed7e9bb62505edb0521
Jean-Marie Renouard <jmrenoaurd@gmail.com>
2021-06-02 00:24:01 +0000

    Adding latency script

---
+++ scripts/bin/tc_latency.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
+inter=${1:-"eth1"}
+latms=${2:-"10000"}
+durs=${3:-"200"}
+
+if [ "$1" = "install" ]; then
+	yum -y install iproute-tc kernel-modules-extra
+	exit $?
+fi
+
+( 
+	set -x
+	tc qdisc add dev $inter root netem delay ${latms}ms; 
+	sleep ${durs}s; 
+	tc qdisc del dev eth1 root 
+) &
