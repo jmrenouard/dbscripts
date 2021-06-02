997d185f3509f8dbbd64f45115043ee1aca754f1
Jean-Marie Renouard <jmrenoaurd@gmail.com>
2021-06-02 21:37:15 +0200

    Adding kill slow queries

--- scripts/bin/kill_slow_queries.sh
+++ scripts/bin/kill_slow_queries.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 [ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+mysql -e 'show processlist' | grep -v 'system user'
 
-for req in $(mysql -Nrs -B -e "select id from information_schema.processlist where user <> 'system user' and TIME >= '0'"); do 
+max_time=${1:-"0"}
+for req in $(mysql -Nrs -B -e "select id from information_schema.processlist where user <> 'system user' and TIME >= '${max_time}'"); do 
 	infos=$(mysql -Nrs -e "select * from information_schema.processlist where id=$req")
-	if [ -n "$infos"]; then
-		ask_yes_no "KILLING $infos"
-		[ $? -eq 0 ] && mysql -e "KILL $id"
+	if [ -n "$infos" ]; then
+		ask_yes_or_no "KILLING $infos"
+		[ $? -eq 0 ] && mysql -v -e "KILL $req"
 	fi
 done
