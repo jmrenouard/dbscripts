afa41757298e8021b229529f6c9329dd705c4be8
Jean-Marie Renouard <jmrenouard@gmail.com>
2021-06-11 16:46:58 +0200

    Support Docker for databases

--- scripts/6_docker/1_install_docker.sh
+++ scripts/6_docker/1_install_docker.sh
@@ -29,7 +29,7 @@ systemctl start docker
 docker images
 docker run hello-world
 
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
 
 (
