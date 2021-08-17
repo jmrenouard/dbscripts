9454c0e786b5f3bf29b4085deec900a81340bdd3
Jean-Marie Renouard <jmrenouard@gmail.com>
2021-08-17 11:24:33 +0200

    Some code cleanup

--- scripts/1_system/reboot_and_wait.sh
+++ scripts/1_system/reboot_and_wait.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-set -x
-id
-whoami
-hostname -a
-pwd
 SRV=$1
-
+CYCLE=${2:-"10"}
 SSH_CMD="ssh -o StrictHostKeyChecking=no ${SRV}"
 
 $SSH_CMD "reboot" & 
@@ -14,7 +9,7 @@ $SSH_CMD "reboot" &
 sleep 5s
 i=0
 
-while [ $i -lt 10 ]; do
+while [ "$i" != "$CYCLE" ]; do
 	$SSH_CMD -q exit 
 	if [ $? -eq 0 ]; then
 		echo "$SRV IS UP AND RUNNING"
@@ -24,7 +19,7 @@ while [ $i -lt 10 ]; do
 	sleep 3s
 	i=$(($i + 1))
 done
-echo "$SRV IS NOT REBOOTED AFTER 31 s"
+echo "$SRV IS NOT REBOOTED AFTER $((1 +$CYCLE * 3)) s"
 exit 3
 
 
