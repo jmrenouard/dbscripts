7e22e8f9d99ed9933de4ad1e658ac3e44acc9a3c
Jean-Marie Renouard <jmrenouard@gmail.com>
2021-09-09 08:54:56 +0200

    NFS Scripts

--- documentation/README.md
+++ documentation/README.md
@@ -10,6 +10,6 @@
  *  [ Installation du serveur MariaDB 10.5 sur OS Centos](./1a_install_mariadb_centos_fr.md)
  *  [ Mise à jour du système](./1_update_fr.md)
  *  [ Configuration du firawall avec firewalld ou iptables](./2_firewall_fr.md)
- *  [ Cionfiguration du serveur MariaDB 10.5 basée sur la RAM et CPU](./3_config_start_fr.md)
+ *  [ Configuration du serveur MariaDB 10.5 basée sur la RAM et CPU](./3_config_start_fr.md)
  *  [ Initialisation du cluster Galera](./5_bootstrap_node_fr.md)
  *  [ Initialisation d un nouveau membre dans le cluster Galera](./6_start_new_node_fr.md)
---
+++ scripts/4_sandbox/5_nfs.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+
+lRC=0
+
+#!/bin/bash
+default_mode=rw
+
+setup_ubuntu_nfs_server()
+{
+    apt -y install nfs-kernel-server nfswatch nfstrace quota
+}
+
+setup_ubuntu_nfs_client()
+{
+    apt -y install nfs-common
+}
+
+setup_centos_nfs_client()
+{
+    yum -y install nfs-utils
+}
+
+createNfsShare()
+{
+    local mp=$1
+    shift
+    mkdir -p $mp
+    chmod 777 $mp
+    chown nobody:nogroup $mp
+
+    sed -i "/$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/exports
+    for ip in $*; do
+                   echo "$mp $ip(${default_mode},sync,no_subtree_check)" >> /etc/exports
+    done
+    exportfs -rav
+    systemctl restart nfs-kernel-server
+}
+
+removeNfsShare()
+{
+    local mp=$1
+    sed -i "/$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/exports
+    exportfs -rav
+    systemctl restart nfs-kernel-server
+}
+
+mountNfsShare()
+{
+    srv=$1
+    mp=$2
+    lmp=$3
+
+    [ -d "$lmp" ] || mkdir -p $lmp
+
+    sed -i "/${srv}:$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/fstab
+
+    df -Ph | grep "$lmp"
+    [ $? -eq 0 ] && umount $lmp
+
+    mount $lmp
+
+    if [ $? -eq 0 ]; then
+                   echo "$srv:$mp    $lmp   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
+    fi
+    df -Ph
+}
+
+umountNfsShare()
+{
+    lmp=$1
+
+    sed -i "/$(echo $lmp| perl -pe 's/\//\\\//g')/d" /etc/fstab
+
+    df -Ph | grep "$lmp"
+    [ $? -eq 0 ] && umount $lmp
+
+    df -Ph
+}
