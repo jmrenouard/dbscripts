54a11344a511393f9c2446b690aa2bfe60d1a478
Jean-Marie Renouard <jean-marie.renouard@prestataire.renater.fr>
2021-10-01 12:43:43 +0200

    Last test for stunnel

--- scripts/4_sandbox/7_stunnel.sh
+++ scripts/4_sandbox/7_stunnel.sh
@@ -4,80 +4,55 @@
 
 lRC=0
 
-#!/bin/bash
-default_mode=rw
-
-setup_ubuntu_stunnel_server()
+push_cert_config()
 {
-    apt update
-    apt -y install nfs-kernel-server nfswatch nfstrace quota stunnel4
+    local target=$1
+    rsync -avz /etc/stunnel/*.pem $target:/etc/stunnel
 }
 
-setup_ubuntu_stunnel_client()
+gen_stunnel_cert()
 {
-    apt update
-    apt -y install nfs-common stunnel4
+    cd /etc/stunnel/
+     rm stunnel.crt stunnel.key stunnel.pem stunnel.csr
+
+    openssl req -newkey rsa:4096 -x509 -days 3650 -nodes \
+  -out stunnel.pem -keyout stunnel.pem
+  chmod 400 stunnel.pem
+
 }
 
-setup_centos_stunnel_client()
+gen_stunnel_server_conf()
 {
-    yum -y install epel-release
-    yum -y install nfs-utils stunnel
+    echo "verify  =       4
+CAfile  =       /etc/stunnel/stunnel.pem
+cert    =       /etc/stunnel/stunnel.pem
+pid = /var/run/stunnel.pid
+output = /var/log/stunnel4/stunnel.log
+
+[nfs_over_stunnel]
+accept = 0.0.0.0:2450
+connect = 127.0.0.1:2049" > /etc/stunnel/stunnel.conf
+chmod 644 /etc/stunnel/stunnel.conf
+systemctl restart stunnel4
+systemctl status stunnel4
+systemctl enable stunnel4
 }
 
-create_stunnel_config()
+gen_stunnel_client_conf()
 {
-    local mp=$1
-    shift
-    mkdir -p $mp
-    chmod 777 $mp
-    chown nobody:nogroup $mp
-
-    sed -i "/$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/exports
-    for ip in $*; do
-                   echo "$mp $ip(${default_mode},sync,no_subtree_check)" >> /etc/exports
-    done
-    exportfs -rav
-    systemctl restart nfs-kernel-server
-}
-
-removestunnelShare()
-{
-    local mp=$1
-    sed -i "/$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/exports
-    exportfs -rav
-    systemctl restart nfs-kernel-server
-}
-
-mountNfsShare()
-{
-    srv=$1
-    mp=$2
-    lmp=$3
-
-    [ -d "$lmp" ] || mkdir -p $lmp
-
-    sed -i "/${srv}:$(echo $mp| perl -pe 's/\//\\\//g')/d" /etc/fstab
-
-    df -Ph | grep "$lmp"
-    [ $? -eq 0 ] && umount $lmp
-
-    mount $lmp
-
-    if [ $? -eq 0 ]; then
-                   echo "$srv:$mp    $lmp   nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" >> /etc/fstab
-    fi
-    df -Ph
-}
-
-umountNfsShare()
-{
-    lmp=$1
-
-    sed -i "/$(echo $lmp| perl -pe 's/\//\\\//g')/d" /etc/fstab
-
-    df -Ph | grep "$lmp"
-    [ $? -eq 0 ] && umount $lmp
-
-    df -Ph
+    local srv=$1
+    echo "verify  =       4
+CAfile  =       /etc/stunnel/stunnel.pem
+cert    =       /etc/stunnel/stunnel.pem
+pid = /var/run/stunnel.pid
+output = /var/log/stunnel4/stunnel.log
+client = yes
+[nfs_over_stunnel]
+accept = 127.0.0.1:2049
+connect = ${srv}:2450
+" > /etc/stunnel/stunnel.conf
+chmod 644 /etc/stunnel/stunnel.conf
+systemctl restart stunnel4
+systemctl status stunnel4
+systemctl enable stunnel4
 }
