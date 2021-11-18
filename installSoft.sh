6e2499057658aeeed796c48c74d9030f0479a7d1
Jean-Marie Renouard <jmrenouard@gmail.com>
2021-11-18 16:17:12 +0100

    Update installSoft.sh
    stop before update

--- installSoft.sh
+++ installSoft.sh
@@ -15,7 +15,7 @@ yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64
 yum install sublime-text kernel-devel kernel-headers gcc make perl wget -y
 
 wget http://download.virtualbox.org/virtualbox/rpm/el/virtualbox.repo -O /etc/yum.repos.d/virtualbox.repo
-
+exit 0
 yum -y install VirtualBox-6.1.x86_64
 yum -y install https://releases.hashicorp.com/vagrant/2.2.16/vagrant_2.2.16_x86_64.rpm
 
