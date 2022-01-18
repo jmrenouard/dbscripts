90c50e70cd6f54948131f8964b9296c00a95879f
Jean-Marie Renouard <jmrenouard@gmail.com>
2022-01-18 15:49:40 -0500

    adding innodb script

---
+++ sql/innodb.sql
@@ -0,0 +1,14 @@
+select TABLE_SCHEMA, ENGINE , TABLE_NAME
+    from information_schema.tables
+    where TABLE_TYPE like 'Base table'
+    AND ENGINE <> 'InnoDB'
+    and TABLE_SCHEMA NOT IN ('mysql', 'performance_schema', 'sys', 'information_schema')
+    ORDER BY TABLE_SCHEMA, ENGINE;
+    
+   SELECT DISTINCT t.table_schema, t.table_name
+       FROM information_schema.tables AS t
+       LEFT JOIN information_schema.columns AS c ON t.table_schema = c.table_schema AND t.table_name = c.table_name
+             AND c.column_key = 'PRI'
+      WHERE t.table_schema NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys')
+        AND c.table_name IS NULL AND t.table_type != 'VIEW';
+   
