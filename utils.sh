65c9a6f4aa00c596206c1c065acd83af3399a66d
Jean-Marie RENOUARD <jean-marie-externe.renouard@datanumia.com>
2022-07-11 17:46:37 +0200

    bytesToHumanReadable function

--- utils.sh
+++ utils.sh
@@ -157,3 +157,13 @@ ppkill()
         echo "---------------------------"
     done
 }
+
+bytesToHumanReadable() {
+    local i=${1:-0} d="" s=0 S=("Bytes" "KiB" "MiB" "GiB" "TiB" "PiB" "EiB" "YiB" "ZiB")
+    while ((i > 1024 && s < ${#S[@]}-1)); do
+        printf -v d ".%02d" $((i % 1024 * 100 / 1024))
+        i=$((i / 1024))
+        s=$((s + 1))
+    done
+    echo "$i$d ${S[$s]}"
+}
