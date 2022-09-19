f3740845c4f2f2494b9cbc7230a4599468119d3a
Jean-Marie Renouard <jmrenouard@gmail.com>
2022-09-19 23:23:02 +0200

    Adding config file for MAxscale

--- scripts/10_proxy/13_maxscale.sh
+++ scripts/10_proxy/13_maxscale.sh
@@ -29,5 +29,83 @@ cmd "cat /etc/yum.repos.d/mariadb_${VERSION}.repo"
 
 cmd "yum -y install $rpm_url"
 
+echo "create user 'maxscale'@'192.168.%' identified by 'maxscale';
+grant select on mysql.user to 'myuser'@'192.168.%';
+GRANT SHOW DATABASES ON *.*
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.columns_priv
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.db
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.proxies_priv
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.procs_priv
+     TO 'mxs'@'192.168.%';
+GRANT SELECT ON mysql.roles_mapping
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.tables_priv
+     TO 'maxscale'@'192.168.%';
+GRANT SELECT ON mysql.user
+     TO 'maxscale'@'192.168.%';"
+
+echo "
+# Globals
+[maxscale]
+threads=1
+ 
+# Servers
+[node0]
+type=server
+address=192.168.56.100
+port=3306
+protocol=MySQLBackend
+ 
+[node1]
+type=server
+address=192.168.56.101
+port=3306
+protocol=MySQLBackend
+ 
+[node2]
+type=server
+address=192.168.56.102
+port=3306
+protocol=MySQLBackend
+ 
+# Monitoring for the servers
+[Galera Monitor]
+type=monitor
+module=galeramon
+servers=node0,node1,node2
+user=maxscale
+passwd=maxscale
+monitor_interval=1000
+ 
+# Galera router service
+[Galera Service]
+type=service
+router=readwritesplit
+servers=node0,node1,node2
+user=maxscale
+passwd=maxscale
+ 
+# MaxAdmin Service
+[MaxAdmin Service]
+type=service
+router=cli
+ 
+# Galera cluster listener
+[Galera Listener]
+type=listener
+service=Galera Service
+protocol=MySQLClient
+port=3306
+ 
+# MaxAdmin listener
+[MaxAdmin Listener]
+type=listener
+service=MaxAdmin Service
+protocol=maxscaled
+socket=default" > /etc/maxscale.cnf
 footer "END SCRIPT: $NAME"
 exit $lRC
