393f1fadab6317981445015eec8628bd5894c125
Jean-Marie Renouard <jmrenouard@gmail.com>
2022-10-05 18:38:29 +0200

    Merge LF

--- sql/innodb.sql
+++ sql/innodb.sql
@@ -4,11 +4,10 @@ select TABLE_SCHEMA, ENGINE , TABLE_NAME
     AND ENGINE <> 'InnoDB'
     and TABLE_SCHEMA NOT IN ('mysql', 'performance_schema', 'sys', 'information_schema')
     ORDER BY TABLE_SCHEMA, ENGINE;
-    
+
    SELECT DISTINCT t.table_schema, t.table_name
        FROM information_schema.tables AS t
        LEFT JOIN information_schema.columns AS c ON t.table_schema = c.table_schema AND t.table_name = c.table_name
              AND c.column_key = 'PRI'
       WHERE t.table_schema NOT IN ('information_schema', 'mysql', 'performance_schema', 'sys')
         AND c.table_name IS NULL AND t.table_type != 'VIEW';
-   
