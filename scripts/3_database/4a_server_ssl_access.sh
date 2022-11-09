613df2b7b1b630471c4720bed687017466097323
Jean-Marie Renouard <jmrenouard@gmail.com>
2022-11-09 16:32:59 +0100

    Reorder code for GlusterFs

--- scripts/3_database/4a_server_ssl_access.sh
+++ scripts/3_database/4a_server_ssl_access.sh
@@ -5,20 +5,23 @@
 
 lRC=0
 CONF_SRV_FILE="/etc/my.cnf.d/99_minimal_ssl_config.cnf"
-CONF_CLI_FILE="/etc/my.cnf.d/50-mysql_ssl_clients.cnf"
 
 banner "BEGIN SCRIPT: $_NAME"
 
 
-mkdir -p /etc/mysql/ssl
+[ -d "/etc/mysql/ssl" ] || mkdir -p /etc/mysql/ssl
+
 cd /etc/mysql/ssl
 
-# CA Key
-openssl genrsa 4096 > ca-key.pem
+if [ ! -f "ca-key.pem" ]; then
+    # CA Key
+    openssl genrsa 4096 > ca-key.pem
+fi
 
-#CA Certificate
-openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem
-
+if [ ! -f "ca-cert.pem" ]; then
+    #CA Certificate
+    openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem
+fi
 
 # Génération des clés pour le serveur
 openssl req -newkey rsa:2048 -days 365000 -nodes -keyout server-key.pem -out server-req.pem
@@ -27,18 +30,10 @@ openssl rsa -in server-key.pem -out server-key.pem
 # Certificat SSL pour le serveur
 openssl x509 -req -in server-req.pem -days 365000 -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
 
-
-# Génération des clés pour le client
-openssl req -newkey rsa:2048 -days 365000 -nodes -keyout client-key.pem -out client-req.pem
-openssl rsa -in client-key.pem -out client-key.pem
-
-# Certificat SSL pour le client
-openssl x509 -req -in client-req.pem -days 365000 -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out client-cert.pem
-
 chown -Rv mysql:root /etc/mysql/ssl/
 
 # Vérification
-openssl verify -CAfile ca-cert.pem server-cert.pem client-cert.pem
+openssl verify -CAfile ca-cert.pem server-cert.pem
 
 echo "[mysqld]
 ssl-ca=/etc/mysql/ssl/ca-cert.pem
@@ -46,15 +41,6 @@ ssl-cert=/etc/mysql/ssl/server-cert.pem
 ssl-key=/etc/mysql/ssl/server-key.pem
 ## Set up TLS version here. For example TLS version 1.2 and 1.3 ##
 tls_version = TLSv1.2,TLSv1.3"| tee $CONF_SRV_FILE
-
-
-echo "[mysql]
-ssl-ca=/etc/mysql/ssl/ca-cert.pem
-ssl-cert=/etc/mysql/ssl/client-cert.pem
-ssl-key=/etc/mysql/ssl/client-key.pem
-"| tee $CONF_CLI_FILE
-/etc/mysql/mariadb.conf.d/50-mysql-clients.cnf
-tar czf client_certificates.tgz ca-cert.pem client-cert.pem client-key.pem
 
 mysql -v -e "FLUSH SSL"
 
---
+++ scripts/3_database/4b_client_ssl_access.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+[ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
+[ -f '/etc/profile.d/utils.mysql.sh' ] && source /etc/profile.d/utils.mysql.sh
+
+lRC=0
+CONF_CLI_FILE="/etc/my.cnf.d/50-mysql_ssl_clients.cnf"
+
+banner "BEGIN SCRIPT: $_NAME"
+
+[ -d "/etc/mysql/ssl" ] || mkdir -p /etc/mysql/ssl
+cd /etc/mysql/ssl
+
+if [ ! -f "ca-key.pem" ]; then
+    # CA Key
+    openssl genrsa 4096 > ca-key.pem
+fi
+
+if [ ! -f "ca-cert.pem" ]; then
+    #CA Certificate
+    openssl req -new -x509 -nodes -days 365000 -key ca-key.pem -out ca-cert.pem
+fi
+
+# Génération des clés pour le client
+openssl req -newkey rsa:2048 -days 365000 -nodes -keyout client-key.pem -out client-req.pem
+openssl rsa -in client-key.pem -out client-key.pem
+
+# Certificat SSL pour le client
+openssl x509 -req -in client-req.pem -days 365000 -CA ca-cert.pem -CAkey ca-key.pem -set_serial 01 -out client-cert.pem
+
+chown -Rv mysql:root /etc/mysql/ssl/
+
+# Vérification
+openssl verify -CAfile ca-cert.pem client-cert.pem
+
+echo "[mysql]
+ssl-ca=/etc/mysql/ssl/ca-cert.pem
+ssl-cert=/etc/mysql/ssl/client-cert.pem
+ssl-key=/etc/mysql/ssl/client-key.pem
+"| tee $CONF_CLI_FILE
+/etc/mysql/mariadb.conf.d/50-mysql-clients.cnf
+
+tar czf client_certificates.tgz ca-cert.pem client-cert.pem client-key.pem
+
+mysql -v -e "FLUSH SSL"
+
+footer "END SCRIPT: $NAME"
+exit $lRC
