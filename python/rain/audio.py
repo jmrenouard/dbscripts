4ba78a82a58c47ffc23e97fb5ed1ee5d44135255
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-01-31 08:33:13 +0100

    Working version for videvo

--- python/rain/audio.py
+++ python/rain/audio.py
@@ -36,14 +36,14 @@ def download_audios(tag=''):
         remaining_audios = 10
     else:
         remaining_audios = int(remaining_audios)
-    logging.debug(f"MAX VIDEOS TO DOWNLOAD: {remaining_audios}")
+    logging.debug(f"MAX AUDIOS TO DOWNLOAD: {remaining_audios}")
 
     per_page = config['VIDEVO_PER_PAGE']
     if per_page is None or per_page == '':
         per_page = 10
     else:
         per_page = int(per_page)
-    
+
     url = f'https://www.videvo.net/search/{stag}/clip_type/royalty-free-sound-effects/'
     while audio_found_flag:
         logging.info('-' * 50)
@@ -53,58 +53,48 @@ def download_audios(tag=''):
         logging.info("Start Videvo search")
         page = requests.get(url)
         soup = BeautifulSoup(page.text, 'html.parser')
-        for mp3link in soup.find_all('a', class_='play-icon'):
-            print(f"Content: {mp3link['audio-source']}")
-        break;
-        os.exit(1)
 
-        if len(audios['audios']) == 0:
+        mp3_urls=[]
+        for mp3link in soup.find_all('a', class_='play-icon'):
+            logging.debug(f"Content: {mp3link['audio-source']}")
+            mp3_urls.append(mp3link['audio-source'])
+        if len(mp3_urls) == 0:
             audio_found_flag = False
             logging.info(f"No more audios")
             break
-        for data in audios['audios']:
-            if data['width'] < data['height'] and config['VIDEVO_ORIENTATION'] == 'horizontal':
-                logging.warning(
-                    f"Videos found: id: {data['id']} SKIPPED FOR BAD ORIENTATION {config['VIDEVO_ORIENTATION']}")
-                continue
-            if data['width'] > data['height'] and config['VIDEVO_ORIENTATION'] == 'vertical':
-                logging.warning(
-                    f"Videos found: id: {data['id']} SKIPPED FOR BAD ORIENTATION {config['VIDEVO_ORIENTATION']}")
-                continue
-            if str(data['id']) not in retrieved_audios:
+        for url_audio in mp3_urls:
+            if url_audio not in retrieved_audios:
                 remaining_audios -= 1
+                basename_audio = os.path.basename(url_audio)
                 # create the url with the audio id
-                url_audio = 'https://www.pexels.com/audio/' + \
-                    str(data['id']) + \
-                    '/download'
                 logging.info('-'*50)
                 r = requests.get(url_audio)
                 logging.info('-'*50)
-                audio_file = os.path.join(result_dir, f"{data['id']}.mp4")
-                logging.info(f"Save audio {data['id']} into {audio_file}")
+                audio_file = os.path.join(result_dir, basename_audio)
+                logging.info(f"Save audio {basename_audio} into {audio_file}")
                 with open(f"{audio_file}", 'wb') as outfile:
                     outfile.write(r.content)
 
-                append_file_content(cache_file, data['id'])
+                append_file_content(cache_file, url_audio)
 
-                json_file = os.path.join(result_dir, f"{data['id']}.json")
+                json_file = f"{audio_file}.json"
                 logging.info(
-                    f"Save audio info {data['id']} into {json_file}")
+                    f"Save audio info {basename_audio} into {json_file}")
                 # Save meta data
-                with open(f"{json_file}", 'wb') as outfile:
-                    outfile.write(json.dumps(data).encode('utf-8'))
+                #with open(f"{json_file}", 'wb') as outfile:
+                #    outfile.write(json.dumps(data).encode('utf-8'))
 
                 logging.info('-'*50)
                 if remaining_audios <= 0:
                     logging.debug(
-                        f"{config['VIDEVO_MAX_AUDIOS']} MAX VIDEOS LIMIT REACHED")
+                        f"{config['VIDEVO_MAX_AUDIOS']} MAX AUDIOS LIMIT REACHED")
                     audio_found_flag = False
                     break
             else:
                 logging.debug(
-                    f"Videos found: id: {data['id']} ALREADY DOWNLOADED")
+                    f"Audios found: id: {url_audio} ALREADY DOWNLOADED")
             logging.debug(
-            f"MAX VIDEOS: {remaining_audios} / {config['VIDEVO_MAX_AUDIOS']}")
+            f"MAX AUDIOS: {remaining_audios} / {config['VIDEVO_MAX_AUDIOS']}")
             random_sleep(int(config['VIDEVO_MAX_WAIT'] or 10))
 
         num_page += 1
