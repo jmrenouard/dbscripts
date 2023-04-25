5173a045a043509cb84ccbefdb8fae9126ef0700
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-04-25 20:32:54 +0200

    decrypt password from MySQL Workbench

---
+++ scripts/bin/decryptWorkbenchPasswords.py
@@ -0,0 +1,4 @@
+import os,win32crypt
+encrypted_data = open(f".\\workbench_user_data.dat", "rb").read()
+clear_data = win32crypt.CryptUnprotectData(encrypted_data, None, None, None, 0)
+print(clear_data)
