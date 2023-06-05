3110a78344d50d52c02136a7b05cc25d01d7e5ff
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-06-05 23:02:34 +0200

    feat: last updates on sysbench scripts

--- scripts/bin/sysbench.sh
+++ scripts/bin/sysbench.sh
@@ -9,18 +9,18 @@ create database sbtest;
 drop user if exists 'sbtest'@'192.168.%';
 drop user if exists 'sbtest'@'localhost';
 
-grant all on sbtest.* to 'sbtest'@'192.168.%'
-identified by '${sbpass}';
-grant all on sbtest.* to 'sbtest'@'localhost'
-identified by '${sbpass}';
+CREATE USER IF NOT EXISTS 'sbtest'@'192.168.%' IDENTIFIED BY '${sbpass}';
+CREATE USER IF NOT EXISTS 'sbtest'@'localhost' IDENTIFIED BY '${sbpass}';
+grant all on sbtest.* to 'sbtest'@'192.168.%';
+grant all on sbtest.* to 'sbtest'@'localhost';
 show grants for 'sbtest'@'192.168.%';
-show grants for 'sbtest'@'localhost';" | mysql -f
+show grants for 'sbtest'@'localhost';" 
 
-add_password_history sbtest $sbpass
+#add_password_history sbtest $sbpass
 }
 
 
-my_sysbench="sysbench --db-driver=mysql --mysql-user=sbtest --mysql_password=$(grep sbtest $HOME/.pass_mariadb| tail -n 1 |awk 'NF{ print $NF }') --mysql-db=sbtest"
+my_sysbench="sysbench --db-driver=mysql --mysql-user=${SYSBENCH_MYSQL_USER:-"sbtest"} --mysql_password=${SYSBENCH_MYSQL_PASSWORD:-"waephei8eihoh2Id"} --mysql-db=sbtest --mysql-port=${SYSBENCH_MYSQL_PORT:-"3306"} --mysql-host=${SYSBENCH_MYSQL_HOST:-"127.0.0.1"}"
 run_load()
 { 
 	nbmin=${1:-"15"}
@@ -28,30 +28,40 @@ run_load()
 	mthreads="2 4 8 16 32 64 128 256 512 1024 1280"
 	mtime=$(($nbmin * 60))
 	(    
-		for scenario in $scenarios; do        
-			for thrd in $mthreads; do        
-				opt_scenario=""        
-				[ "$scenario" != "write_only" ] && opt_scenario=" --skip_trx=on"          
-				echo "-------------------------------------------------------"          
-				echo "SCENARIO $scenario WITH $thrd THREADS FOR ${mtime} sec.($nbmin min.)"          
-				echo "-------------------------------------------------------"          
-				date          
-				echo "-------------------------------------------------------"          
-				tmpScript=$(mktemp)          
+		for scenario in $scenarios; do
+			for thrd in $mthreads; do
+				opt_scenario=""
+				[ "$scenario" != "write_only" ] && opt_scenario=" --skip_trx=on"
+				echo "-------------------------------------------------------"
+				echo "SCENARIO $scenario WITH $thrd THREADS FOR ${mtime} sec.($nbmin min.)"
+				echo "-------------------------------------------------------"
+				date
+				echo "-------------------------------------------------------"
+				tmpScript=$(mktemp)
 				echo "#!/bin/bash
 				source $HOME/.bash_profile
 				my_sysbench --threads=$thrd --tables=16 --rate=10 --table-size=10000 --time=$mtime /usr/share/sysbench/oltp_${scenario}.lua run" > $tmpScript
 				chmod 755 $tmpScript
-				nohup /bin/bash $tmpScript           
-				echo "-------------------------------------------------------"         
-				date          
-				echo "-------------------------------------------------------"          
-				rm -f $tmpScript        
+				nohup /bin/bash $tmpScript 
+				echo "-------------------------------------------------------" 
+				date
+				echo "-------------------------------------------------------"
+				rm -f $tmpScript
 			done    
 		done 2>&1
 	) | tee run_load_${nbmin}_$(date +%Y%m%d_%H%M).log}
 }
 
+simple_run_docker()
+{
+	gen_opts="--db-driver=mysql --mysql-user=sbtest --mysql_password=waephei8eihoh2Id --mysql-db=sbtest --mysql-host=192.168.68.56 --threads=2 --time=120 --report-interval=2 --tables=5 --table-size=100000 /usr/share/sysbench/oltp_read_write.lua"
+	set -x
+	for port in 3307 3308; do
+	  sysbench --mysql-port=$port $gen_opts cleanup
+  	  sysbench --mysql-port=$port $gen_opts prepare
+	  sysbench --mysql-port=$port $gen_opts run
+	done
+}
 echo "$my_sysbench --tables=120 --table-size=1000000 /usr/share/sysbench/oltp_read_write.lua cleanup
 $my_sysbench --tables=120 --table-size=1000000 /usr/share/sysbench/oltp_read_write.lua prepare
 
