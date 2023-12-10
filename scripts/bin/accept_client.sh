adbedb8d056c5e9c1d5d5269c3a076d8068eaa8b
Jean-Marie Renouard <jmrenouard@gmail.com>
2023-12-10 23:21:05 +0100

    Last updates from consulting sessions

--- scripts/bin/accept_client.sh
+++ scripts/bin/accept_client.sh
@@ -2,7 +2,8 @@
 
 [ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
 
-for rule_id in $(/sbin/iptables --list --line-numbers | grep -E 'REJECT.*(mysql|3306)' | awk '{print $1}'); do
-	/sbin/iptables -D INPUT $rule_id
+export PATH=/sbin:$PATH
+for rule_id in $(iptables --list --line-numbers | grep -E 'REJECT.*(mysql|3306)' | awk '{print $1}'); do
+	iptables -D INPUT $rule_id
 done
-/sbin/iptables -L
+iptables -L
--- scripts/bin/block_client.sh
+++ scripts/bin/block_client.sh
@@ -3,6 +3,7 @@
 
 [ -f '/etc/profile.d/utils.sh' ] && source /etc/profile.d/utils.sh
 
-/sbin/iptables -A INPUT -p tcp --dport 3306 -j REJECT
+export PATH=/sbin:$PATH
+iptables -A INPUT -p tcp --dport 3306 -j REJECT
 
-/sbin/iptables -L
+iptables -L
