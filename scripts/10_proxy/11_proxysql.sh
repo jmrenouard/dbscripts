b215b7562013cb6fd6d704e212fab66ba81816c4
Jean-Marie Renouard <jmrenouard@gmail.com>
2024-03-08 23:19:45 +0100

    update last proxySQL

--- scripts/10_proxy/11_proxysql.sh
+++ scripts/10_proxy/11_proxysql.sh
@@ -8,9 +8,9 @@ myrootmy1
 banner "BEGIN SCRIPT: $_NAME"
 
 if [ "$ID" = "ubuntu" ]; then
-        cmd "apt -y install https://github.com/sysown/proxysql/releases/download/v2.1.1/proxysql_2.1.1-ubuntu20_arm64.deb"
+        cmd "apt -y install https://github.com/sysown/proxysql/releases/download/v2.6.0/proxysql_2.6.0-ubuntu22_amd64.deb"
 else 
-        cmd "yum -y install https://github.com/sysown/proxysql/releases/download/v2.1.1/proxysql-2.1.1-1-centos8.x86_64.rpm"
+        cmd "yum -y install https://github.com/sysown/proxysql/releases/download/v2.6.0/proxysql-2.6.0-1-centos7.x86_64.rpm"
 fi
 
 cmd "systemctl enable haproxy"
@@ -20,6 +20,7 @@ firewall-cmd --add-port=6033/tcp --permanent
 firewall-cmd --add-port=6032/tcp --permanent
 firewall-cmd --add-port=6080/tcp --permanent
 firewall-cmd --reload
+
 
 footer "END SCRIPT: $NAME"
 exit $lRC
