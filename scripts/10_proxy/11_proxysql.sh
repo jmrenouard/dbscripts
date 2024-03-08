a5ce592eefe8c81200a81f6bf22f5305e44a6ee0
Jean-Marie Renouard <jmrenouard@gmail.com>
2024-03-08 23:34:36 +0100

    Adding default client for mysql/maraiadb

--- scripts/10_proxy/11_proxysql.sh
+++ scripts/10_proxy/11_proxysql.sh
@@ -8,7 +8,8 @@ myrootmy1
 banner "BEGIN SCRIPT: $_NAME"
 
 if [ "$ID" = "ubuntu" ]; then
-        cmd "wget https://github.com/sysown/proxysql/releases/download/v2.6.0/proxysql_2.6.0-ubuntu22_amd64.deb"
+        cmd "rm -f proxysql_2.6.0-ubuntu22_amd64.deb*"
+				cmd "wget https://github.com/sysown/proxysql/releases/download/v2.6.0/proxysql_2.6.0-ubuntu22_amd64.deb"
 				cmd "dpkg -i proxysql_2.6.0-ubuntu22_amd64.deb"
 else 
         cmd "yum -y install https://github.com/sysown/proxysql/releases/download/v2.6.0/proxysql-2.6.0-1-centos7.x86_64.rpm"
@@ -19,6 +20,7 @@ cmd "systemctl stop haproxy"
 cmd "systemctl enable proxysql"
 cmd "systemctl start proxysql"
 
+cmd "apt -y install mariadb-client-core-10.6" 
 which firewall-cmd &>/dev/null
 if [ $? -eq 0 ]; then
 	firewall-cmd --add-port=6033/tcp --permanent
