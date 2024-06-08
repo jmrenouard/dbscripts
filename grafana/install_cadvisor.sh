6091982d0045c37be3c31173af931eb95a25eb82
Jean-Marie Renouard <jmrenouard@gmail.com>
2024-06-08 17:26:51 +0000

    Installer scrtipt

---
+++ grafana/install_cadvisor.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+version="${VERSION:-"0.49.1"}"
+arch="${ARCH:-"linux-amd64"}"
+bin_dir="${BIN_DIR:-/usr/local/bin}"
+
+mkdir -p /usr/local/bin
+wget "https://github.com/google/cadvisor/releases/download/v${version}/cadvisor-v${version}-${arch} \
+-O ${bin_dir}/cadvisor
+
+chown root:staff $bin_dir/*
+chmod 755 $bin_dir/*
+
+systemctl disable cadvisor.service
+systemctl stop cadvisor.service
+
+cat <<EOF > /etc/systemd/system/cadvisor.service
+[Unit]
+Description=Prometheus cadvisor exporter
+After=local-fs.target network-online.target network.target
+Wants=local-fs.target network-online.target network.target
+
+[Service]
+Type=simple
+ExecStartPre=-/sbin/iptables -I INPUT 1 -p tcp --dport 9202 -j ACCEPT
+ExecStartPre=-/sbin/iptables -I INPUT 3 -p tcp --dport 9202 -j DROP
+ExecStart=/usr/local/bin/cadvisor --port 9202
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl daæon-reload
+systemctl enable cadvisor.service
+systemctl start cadvisor.service
+
+echo "SUCCESS! Installation succeeded!"
