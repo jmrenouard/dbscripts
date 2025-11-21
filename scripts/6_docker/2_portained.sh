db45c212b5750aba157e27b6349ad8c1fef3ef7e
Jean-Marie Renouard <jmrenouard@gmail.com>
2025-11-21 19:14:00 +0100

    fix: Add missing --privileged flag in Portainer Docker run command

--- scripts/6_docker/2_portained.sh
+++ scripts/6_docker/2_portained.sh
@@ -17,5 +17,6 @@ docker run -d -p 9000:9000 -p 8000:8000 \
 --name=portainer --restart=always \
 -v /run/docker.sock:/var/run/docker.sock \
 -v portainer_data:/data \
+--privileged \
 -v /var/tmp/portainer_password:/var/tmp/portainer_password portainer/portainer-ce \
 --admin-password-file /var/tmp/portainer_password
