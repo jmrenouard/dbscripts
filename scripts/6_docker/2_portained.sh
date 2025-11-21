57ba450e6e9467efe4bb226f7a8d39ff313e690e
Jean-Marie Renouard <jmrenouard@gmail.com>
2025-11-21 18:53:50 +0100

    fix: Correct volume mount syntax in Portainer setup script

--- scripts/6_docker/2_portained.sh
+++ scripts/6_docker/2_portained.sh
@@ -15,8 +15,7 @@ echo -n "portainer" > /var/tmp/portainer_password
 
 docker run -d -p 9000:9000 -p 8000:8000 \
 --name=portainer --restart=always \
--v /var/run/docker.sock:/var/run/docker.sock \
+-v /run/docker.sock:/var/run/docker.sock \
 -v portainer_data:/data \
 -v /var/tmp/portainer_password:/var/tmp/portainer_password portainer/portainer-ce \
 --admin-password-file /var/tmp/portainer_password
-
